50d0660d0cf3c1647bbcd75254f808f6f377f15f
Totoro <benjahi50@gmail.com>
2017-07-24 16:56:24 -0500

    first

---
+++ final project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  final project
+//
+//  Created by Ben Sparks on 7/24/17.
+//  Copyright © 2017 bdsc7d. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
