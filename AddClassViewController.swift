774d80f06d440b901fd444e1748b62c0f0a8ded5
Totoro <benjahi50@gmail.com>
2017-07-24 19:09:14 -0500

    UI

--- AddClassViewController.swift
+++ AddClassViewController.swift
@@ -1,25 +1,16 @@
-//
-//  ViewController.swift
-//  final project
-//
-//  Created by Ben Sparks on 7/24/17.
-//  Copyright © 2017 bdsc7d. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class AddClassViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
